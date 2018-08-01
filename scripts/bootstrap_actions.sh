975fc31362cac50760bf22231c4f7c830b0b9515
Roshan Bangera <roshan.bangera@turner.com>
2018-08-01 19:53:08 -0400

    * configure sagemaker_pyspark on emr cluster

--- scripts/bootstrap_actions.sh
+++ scripts/bootstrap_actions.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # install conda
-wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
-    && /bin/bash ~/miniconda.sh -b -p $HOME/conda
+#wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
+#    && /bin/bash ~/miniconda.sh -b -p $HOME/conda
 
-echo -e '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
+#echo -e '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
 
 # install packages
-conda install -y ipython jupyter
+#conda install -y ipython jupyter boto3
+
+sudo pip install sagemaker_pyspark
+sudo /usr/bin/pip-3.4 install sagemaker_pyspark
--- scripts/pyspark_quick_setup.sh
+++ scripts/pyspark_quick_setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # bind conda to spark
-echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
-echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> /etc/spark/conf/spark-env.sh
+#echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
+#echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
+#echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> /etc/spark/conf/spark-env.sh
+
+SAGEPYSPARK_JARS=`/usr/local/bin/sagemakerpyspark-emr-jars :`
+
+cd /usr/lib/spark/conf
+cp spark-defaults.conf spark-defaults.conf.bk
+sed -i "/^spark\.driver\.extraClassPath/ s|\$|:$SAGEPYSPARK_JARS|" spark-defaults.conf
+sed -i "/^spark\.executor\.extraClassPath/ s|\$|:$SAGEPYSPARK_JARS|" spark-defaults.conf
